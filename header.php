ff3dd8474a3a4f7c78ee0ee642781a12913f26f2
Kyrie Tompkins-Overlock <kyrie@overlockdesign.co>
2020-03-31 17:08:06 -0400

    Begin Menu Styles

--- header.php
+++ header.php
@@ -45,8 +45,8 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'yourspace' ); ?></button>
+		<nav id="site-navigation" class="primary-navigation">
+			<button class="menu-toggle" aria-controls="mobile-menu" aria-expanded="false"><?php esc_html_e( 'Mobile Menu', 'yourspace' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'primary-menu',
