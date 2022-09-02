a72b626fcf5e7c0fbe8e780dc252a26c427ac543
Kyrie Tompkins-Overlock <kyrie@overlockdesign.co>
2022-09-02 13:44:32 -0400

    Fix wrapping on long site titles; Bump Version No.

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@
     
 ****************************************************************/
 
-define( 'THEME_VERSION', '1.0.4' );
+define( 'THEME_VERSION', '1.0.5' );
 
 if ( ! function_exists( 'yourspace_setup' ) ) :
 
