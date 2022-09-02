6a7f9e5504dd6dded697609b4ad1f630e835d46d
Kyrie Tompkins-Overlock <kyrie@overlockdesign.co>
2022-09-02 12:59:44 -0400

    Add Full-Width Page Template

---
+++ page_full.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Full Width
+ *
+ * This template has no sidebar.
+ *
+ * @package YourSpace
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
